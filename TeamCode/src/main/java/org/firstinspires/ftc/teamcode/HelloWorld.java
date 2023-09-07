2b6e4b813253c2537f80c63e63c2e053bb2e5fa3
Ike <issac_palmer.stu@willowschoolnola.org>
2023-09-07 11:19:36 -0500

    helloworld fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -3,16 +3,16 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-public class Joey4A {
-}
 @TeleOp()
-public class HelloWorldCommented extends OpMode {
-    @Override
-    public void init() {
+public class HelloWorld extends OpMode {
+@Override
+public void init() {
         telemetry.addData("Hello","World");
-    }
-    @Override
-    public void loop() {
 
-    }
-}
+        }
+
+@Override
+public void loop() {
+
+        }
+        }
