32f7723e6489a91ff8a8d6469f78937d62fe0dc1
SalBalsamo <Sal_Balsamo.stu@willowschoolnola.org>
2023-09-15 11:04:06 -0500

    1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zen4A.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zen4A.java
@@ -7,14 +7,3 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class Zen4A {
 }
 
-@TeleOp()
-public class HelloWorldCommented Extends OpMode {
-    @Override
-    public void init() {
-        Telemetry.addData("Hello", "World");
-        @Override
-        public void loop () {
-        }
-    }
-
-
