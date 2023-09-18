b9b74a4b885eee55e2ca78ba661501b4a4957a7a
SalBalsamo <Sal_Balsamo.stu@willowschoolnola.org>
2023-09-18 11:22:47 -0500

    Update Zen4A.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zen4A.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zen4A.java
@@ -5,59 +5,59 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.tel
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 
 @TeleOp
 public class Zen4A extends OpMode {
+    private DcMotor motor1;
     double Speed = -gamepad1.left_stick_y * 0.5;
     double X = gamepad1.right_stick_x;
     double Y = gamepad1.right_stick_y;
 
     @Override
-    public void init(){
+    public void init() {
+        motor1  = hardwareMap.get(DcMotor.class, "testMotor1");
     }
 
     @Override
-    public  void loop(){
-        if(gamepad1.left_stick_y < -0.5){
+    public void loop() {
+        if (gamepad1.left_stick_y < -0.5) {
             telemetry.addData("Left stick", " is negative and large");
-        }
-
-        else if (gamepad1.left_stick_y < 0){
+        } else if (gamepad1.left_stick_y < 0) {
             telemetry.addData("Left stick", " is negative and small");
-        }
-
-        else if (gamepad1.left_stick_y < 0.5){
+        } else if (gamepad1.left_stick_y < 0.5) {
             telemetry.addData("Left stick", " is positive and small");
-        }
-
-        else {
+        } else {
             telemetry.addData("Left stick", " is positive and large");
         }
 
         //Turbo Mode
-        if (gamepad1.a){
+        if (gamepad1.a) {
             Speed = Speed * 2;
         } else {
             Speed = 0.5;
         }
+        //if driver presses b press motor `
+        if (gamepad1.b) {
+            motor1.setPower(1);
+        }
 
         //Crazy mode
-        if (gamepad1.a){
-            if(gamepad1.right_stick_y > 0){
+        if (gamepad1.a) {
+            if (gamepad1.right_stick_y > 0) {
                 telemetry.addData("Y ", X);
 
             }
 
-            if(gamepad1.right_stick_x > 0){
+            if (gamepad1.right_stick_x > 0) {
                 telemetry.addData("X", Y);
             }
-        }
-
-        else{
+        } else {
             telemetry.addData("X", X);
             telemetry.addData("Y", Y);
         }
     }
+}
